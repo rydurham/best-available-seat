60db17b2730c22a46d4abf1a7e8b54274d1827f6
Ryan C. Durham <ryan@stagerightlabs.com>
2021-03-09 12:52:10 -0800

    initial commit. Application scaffolding.

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: {
+    content: [
+      './resources/**/*.html',
+      './resources/**/*.php'
+    ],
+    options: {
+      defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+      whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
